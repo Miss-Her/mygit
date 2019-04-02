814fcd5846021bca795bc0ddfb042e7d0e44e7af
hmp <hmp1836608@163.com>
2019-04-02 11:01:22 +0800

    提交了一个项目miaosha

---
+++ miaosha/src/main/java/com/miaoshaProject/controller/viewObject/UserVO.java
@@ -0,0 +1,61 @@
+package com.miaoshaProject.controller.viewObject;
+
+/**
+ * Package:com.miaoshaProject.controller.viewObject
+ * ClassName:UserVO
+ *
+ * @author:hmp
+ * @date:2019/3/30 20:36
+ * Description:
+ */
+public class UserVO {
+    private Integer id;
+
+    private String name;
+
+    private Byte gender;
+
+    private Integer age;
+
+    private String telphone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Byte getGender() {
+        return gender;
+    }
+
+    public void setGender(Byte gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+}
